ddf8dac904a3b9ea8236cf79867232d79a8ce2c0
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2023-04-28 10:59:36 +1200

    feat: adding click animation for tiles and bolder borders as well

--- battleship/src/components/Hit/Marker/index.tsx
+++ battleship/src/components/Hit/Marker/index.tsx
@@ -3,9 +3,6 @@ import { useSelector } from "react-redux";
 import { makeSelectHit } from "../../../state/hits";
 
 export const HitMarker = ({ id }: HitMarkerProps) => {
-	if (typeof id !== typeof 0)
-		throw new Error("HitMarker must have an identifier");
-
 	const isHit = useSelector(makeSelectHit(id as number));
 
 	if (!isHit) return <span>Not Hit!</span>;
