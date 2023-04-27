d38227d3c78bc9bc221e07a2dc0f2613497767d9
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2023-04-27 18:51:27 +1200

    refactor: moving constant stuff outside the render function, changing from the default export to named export

--- battleship/src/App.tsx
+++ battleship/src/App.tsx
@@ -7,41 +7,37 @@ import { Tile } from "./components/Tile";
 import { HitContainer } from "./components/Hit/Container";
 import { HitMarker } from "./components/Hit/Marker";
 
-function App() {
-  const ROWS = 10;
-  const COLUMNS = 10;
+const ROWS = 10;
+const COLUMNS = 10;
 
-  const grid = new CartesianGrid(ROWS, COLUMNS);
+const grid = new CartesianGrid(ROWS, COLUMNS);
 
-  const points = new Array(ROWS * COLUMNS).fill(null).map(() => grid.next);
-  const positions = getShipPositions(SHIP_LAYOUT);
+const points = new Array(ROWS * COLUMNS).fill(null).map(() => grid.next);
+const positions = getShipPositions(SHIP_LAYOUT);
 
-  return (
-    <Provider store={globalStore}>
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="grid grid-rows-2 md:grid-rows-0 md:grid-cols-2 my-12">
-          <div className="max-w-md">
-            <HitContainer>
-              {positions.map((position) => (
-                <HitMarker key={position.id} id={position.id} />
-              ))}
-            </HitContainer>
-          </div>
+export const App = () => (
+  <Provider store={globalStore}>
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="grid grid-rows-2 md:grid-rows-0 md:grid-cols-2 my-12">
+        <div className="max-w-md">
+          <HitContainer>
+            {positions.map((position) => (
+              <HitMarker key={position.id} id={position.id} />
+            ))}
+          </HitContainer>
+        </div>
 
-          <div className="w-full">
-            <TileContainer shipPositions={positions}>
-              {points.map((point) => (
-                <Tile
-                  key={TileIdentifier.instance.next(point)}
-                  coordinates={point as Point}
-                />
-              ))}
-            </TileContainer>
-          </div>
+        <div className="w-full">
+          <TileContainer shipPositions={positions}>
+            {points.map((point) => (
+              <Tile
+                key={TileIdentifier.instance.next(point)}
+                coordinates={point as Point}
+              />
+            ))}
+          </TileContainer>
         </div>
       </div>
-    </Provider>
-  );
-}
-
-export default App;
+    </div>
+  </Provider>
+);
