5b3edb53a8911cac49ced6a206bf080ea81ff065
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2023-04-28 15:57:58 +1200

    fix: remove span wrapping PlayerCardScore

--- battleship/src/components/Player/Card/index.tsx
+++ battleship/src/components/Player/Card/index.tsx
@@ -7,9 +7,7 @@ export const PlayerCard = ({
 }: PlayerCardProps) => (
 	<div
 		className={`flex aspect-[4/3] w-full flex-col items-center justify-items-center space-y-4 px-4 py-8 ${className}`}>
-		<span className="font-bold text-[#4a4a4a]">
-			<PlayerCardScore type={type} />
-		</span>
+		<PlayerCardScore type={type} />
 		<div className="h-[0.8px] w-3/4 bg-[#4a4a4a]" />
 		<span className="text-center text-2xl font-bold text-[#4a4a4a]">
 			{label}
