32da75c5cc92431c9bbb4f0a5e1aaf163a48cd86
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2023-04-28 20:28:17 +1200

    fix: removing bottom margin from amber side of layout

--- battleship/src/layouts/index.tsx
+++ battleship/src/layouts/index.tsx
@@ -1,7 +1,7 @@
 export const Layout = ({ children }: LayoutProps) => (
 	<>
 		<div className="mx-auto flex max-w-7xl justify-center bg-amber-500 px-4 sm:px-6 lg:px-8">
-			<div className="my-4 grid w-8/12 grid-cols-1 sm:my-6 lg:my-8 lg:grid-cols-2">
+			<div className="mt-4 grid w-8/12 grid-cols-1 sm:mt-6 lg:mt-8 lg:grid-cols-2">
 				<div className="w-full">{children?.[2]}</div>
 
 				<div className="max-w-3xl">{children?.[0]}</div>
