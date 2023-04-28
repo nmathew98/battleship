e022d41193ed39252447a0a96404e7b7a68b7707
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2023-04-28 15:55:41 +1200

    fix: aspect ratio for cards >= md

--- battleship/src/components/Player/Card/index.tsx
+++ battleship/src/components/Player/Card/index.tsx
@@ -6,12 +6,14 @@ export const PlayerCard = ({
 	label,
 }: PlayerCardProps) => (
 	<div
-		className={`flex w-full flex-col items-center justify-items-center space-y-4 px-4 py-8 ${className}`}>
-		<span className="text-center text-4xl font-bold text-[#4a4a4a]">
+		className={`flex aspect-[4/3] w-full flex-col items-center justify-items-center space-y-4 px-4 py-8 ${className}`}>
+		<span className="font-bold text-[#4a4a4a]">
 			<PlayerCardScore type={type} />
 		</span>
 		<div className="h-[0.8px] w-3/4 bg-[#4a4a4a]" />
-		<span className="text-center font-bold text-[#4a4a4a]">{label}</span>
+		<span className="text-center text-2xl font-bold text-[#4a4a4a]">
+			{label}
+		</span>
 	</div>
 );
 
